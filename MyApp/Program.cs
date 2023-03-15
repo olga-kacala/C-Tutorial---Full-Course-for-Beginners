1f348db3628346116d78b6c01e19ee93ce6c6816
olga-kacala <olga.kacala@gmail.com>
2023-03-15 19:24:28 +0100

    String methods exercise done

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,30 @@ namespace Panda
     {
         static void Main(string[] args)
         {
-        string characterName = "John";
-        int characterAge = 35;
+        // string characterName = "John";
+        // int characterAge = 35;
         
-            Console.WriteLine("There was a man named " + characterName);
-            Console.WriteLine(characterName + " was "+ characterAge);
-            characterName = "Mike";
-            Console.WriteLine(characterName + " liked beeing " + characterAge);
+        //     Console.WriteLine("There was a man named " + characterName);
+        //     Console.WriteLine(characterName + " was "+ characterAge);
+        //     characterName = "Mike";
+        //     Console.WriteLine(characterName + " liked beeing " + characterAge);
+
+
+        string phrase = "My course";
+        char grade = 'A';
+        int age = 30;
+        double gpa = 0.3;
+        bool isMale = true;
+
+        Console.WriteLine("My\ncourse");
+        Console.WriteLine(phrase.Length);
+        Console.WriteLine(phrase.ToUpper());
+        Console.WriteLine(phrase.Contains("My"));
+        Console.WriteLine(phrase[0]);
+        Console.WriteLine(phrase.IndexOf("course"));
+        Console.WriteLine(phrase.IndexOf("o"));
+        Console.WriteLine(phrase.Substring(3, 6));
+         
 
             Console.ReadLine();
         }
