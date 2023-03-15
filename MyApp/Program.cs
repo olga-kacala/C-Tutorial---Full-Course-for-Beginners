f7b56b2b48ab40553479794ea3340d3c037e81f9
olga-kacala <olga.kacala@gmail.com>
2023-03-15 18:36:13 +0100

    Creating MyApp with first Program.cs file

---
+++ MyApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string characterName = "John";
+        int characterAge = 35;
+        
+            Console.WriteLine("There was a man named " + characterName);
+            Console.WriteLine(characterName + " was "+ characterAge);
+            characterName = "Mike";
+            Console.WriteLine(characterName + " liked beeing " + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
