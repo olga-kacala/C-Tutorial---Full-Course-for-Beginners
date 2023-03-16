fb5a6619f74a85809db577067286bac50af8d7e4
olga-kacala <olga.kacala@gmail.com>
2023-03-16 14:31:30 +0100

    Done a calculator and mad libs code

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -33,20 +33,39 @@ namespace Panda
         // Console.WriteLine(phrase.ToUpper());
         // Console.WriteLine(phrase.Contains("My"));
         // Console.WriteLine(phrase[0]);
-        // Console.WriteLine(phrase.IndexOf("course"));
+        // Console.WriteLine(phrase.IndexOf("course"));clear
         // Console.WriteLine(phrase.IndexOf("o"));
         // Console.WriteLine(phrase.Substring(3, 6));
         // Console.WriteLine(Math.Sqrt(36));
         //     Console.ReadLine();
 
-Console.Write("Enter your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("Hello "+ name);
-Console.Write("What is your are?");
-string age = Console.ReadLine();
-Console.WriteLine("Your age is "+ age);
-Console.ReadLine(); 
+// Console.Write("Enter your name: ");
+// string name = Console.ReadLine();
+// Console.WriteLine("Hello "+ name);
+// Console.Write("What is your are?");
+// string age = Console.ReadLine();
+// Console.WriteLine("Your age is "+ age);
+// Console.ReadLine(); 
 
+// Console.Write("Type a number: ");
+// double num1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Type second number: ");
+// double num2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine(num1+num2);
+// Console.ReadLine();
+
+string colour, noun, celebrity;
+
+Console.Write("Type a colour: ");
+colour = Console.ReadLine();
+Console.Write("Type a noun: ");
+noun = Console.ReadLine();
+Console.Write("Type a celebrity: ");
+celebrity = Console.ReadLine();
+Console.WriteLine("Roses are " + colour);
+Console.WriteLine(noun + " are blue");
+Console.WriteLine("I love " + celebrity);
+Console.ReadLine();
 
         }
     }
